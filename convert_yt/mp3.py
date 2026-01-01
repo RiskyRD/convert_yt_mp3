bab7e45e3e924294d6fb2b54e1d23f6c97843d1f
RiskyRD <kadekriskyraju25@gmail.com>
2026-01-01 22:24:53 +0800

    First commit

---
+++ convert_yt/mp3.py
@@ -0,0 +1,15 @@
+import yt_dlp
+
+url = "https://youtu.be/YourYTLinkHere"
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
